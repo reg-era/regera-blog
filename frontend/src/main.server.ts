3288edbf423e3e9253f2c3f2afaf478635601e03
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-09-16 23:30:15 +0100

    cleaning and sync the front end

--- frontend/src/main.server.ts
+++ frontend/src/main.server.ts
@@ -1,7 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
-
-export default bootstrap;
+export default (context: BootstrapContext) => bootstrapApplication(App, config, context);
--- postgresconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE IF NOT EXISTS example (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL
-    );
-EOSQL
